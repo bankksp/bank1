5ec05aaa82115c127cf2a3d9b9fbd27b641d984c
bankksp <nanthaphat.bn@gmail.com>
2026-01-12 23:09:11 +0700

    Update SchoolLogo.tsx

--- components/SchoolLogo.tsx
+++ components/SchoolLogo.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const SchoolLogo: React.FC<React.ImgHTMLAttributes<HTMLImageElement>> = (props) => {
   const logoUrl = 'https://img5.pic.in.th/file/secure-sv1/-300x30052b1d65e147cbf32.png';
-  return <img src={logoUrl} alt="โลโก้โรงเรียนกาฬสินธุ์ปัญญานุกูล" {...props} />;
+  return <img src={logoUrl} alt="โลโก้" {...props} />;
 };
 
 export default SchoolLogo;
